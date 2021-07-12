60d6ff962863c2bd926b3adf76c4a8f5f264df53
jiayuewu16 <jwu7@caltech.edu>
2021-07-12 13:39:31 -0700

    Set up back4app and Parse database.

---
+++ app/src/main/java/com/example/taskify/ParseApplication.java
@@ -0,0 +1,35 @@
+package com.example.taskify;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Use for troubleshooting -- remove this line for production
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See https://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        // set applicationId, and server server based on the values in the back4app settings.
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(BuildConfig.BACK4APP_APP_ID) // should correspond to Application Id env variable
+                .clientKey(BuildConfig.BACK4APP_CLIENT_KEY)  // should correspond to Client key env variable
+                .server(getResources().getString(R.string.back4app_server_url)).build());
+    }
+}
+
