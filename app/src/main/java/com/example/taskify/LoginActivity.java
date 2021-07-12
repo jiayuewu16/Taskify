2d33fa100bf7c5feb7280782de4ea093880cc31f
jiayuewu16 <jwu7@caltech.edu>
2021-07-12 14:35:49 -0700

    User can log in and sign up.

---
+++ app/src/main/java/com/example/taskify/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.taskify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.taskify.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        binding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logInInBackground(binding.editTextUsername.getText().toString(), binding.editTextPassword.getText().toString(), new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            // The user is logged in.
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Log in didn't succeed. Either username doesn't exist or password is incorrect.
+                            binding.textViewLoginError.setText(R.string.login_error);
+                        }
+                    }
+                });
+            }
+        });
+
+        binding.buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser user = new ParseUser();
+                // Set core properties
+                user.setUsername(binding.editTextUsername.getText().toString());
+                user.setPassword(binding.editTextPassword.getText().toString());
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Sign up didn't succeed. Username is taken.
+                            binding.textViewLoginError.setText(R.string.signup_error);
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
