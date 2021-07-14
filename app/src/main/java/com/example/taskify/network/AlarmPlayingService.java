ad583fda6a7322138a14dbd328bbbf59f3ca7baa
jiayuewu16 <jwu7@caltech.edu>
2021-07-14 16:57:55 -0700

    Custom logo; notification can be dismissed by tapping/swiping on it.

--- app/src/main/java/com/example/taskify/network/AlarmPlayingService.java
+++ app/src/main/java/com/example/taskify/network/AlarmPlayingService.java
@@ -6,13 +6,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
-
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
-
 import com.example.taskify.R;
 import com.example.taskify.activities.MainActivity;
 import com.example.taskify.models.Task;
@@ -21,10 +17,6 @@ public class AlarmPlayingService extends Service {
     // Tutorial: https://www.c-sharpcorner.com/article/create-alarm-android-application/
 
     private NotificationManager notificationManager;
-
-    private MediaPlayer mediaSong;
-    private int startId;
-    private boolean isRunning;
 
     @Nullable
     @Override
@@ -47,40 +39,22 @@ public class AlarmPlayingService extends Service {
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Intent activityIntent = new Intent(getApplicationContext(), MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
         String NOTIFICATION_CHANNEL_ID = "com.example.taskify";
         String channelName = "Taskify";
+        int notificationId = 166;
         createNotificationChannel(NOTIFICATION_CHANNEL_ID, channelName);
 
         String alarmTitle = intent.getStringExtra(Task.KEY_TASK_NAME);
         Notification notificationPopup = new Notification.Builder(this, NOTIFICATION_CHANNEL_ID)
-                .setOngoing(true)
                 .setContentTitle(alarmTitle)
-                .setSmallIcon(R.drawable.ic_taskify_logo)
+                .setSmallIcon(R.drawable.ic_taskify_logo_transparent_white)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
                 .build();
-        startForeground(1, notificationPopup);
+        startForeground(notificationId, notificationPopup);
+        stopForeground(false);
 
-        startId = 1;
-
-        if (!this.isRunning && startId == 1) {
-            this.isRunning = true;
-            this.startId = 0;
-
-            mediaSong = MediaPlayer.create(this, R.raw.bradbreeckmelody);
-            mediaSong.start();
-        }
-        else {
-            this.isRunning = false;
-            this.startId = 0;
-        }
         return START_NOT_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        // Tell the user we stopped.
-        super.onDestroy();
-        this.isRunning = false;
     }
 }
